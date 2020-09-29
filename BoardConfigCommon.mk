1d0dbefdca08488030085c1dc41f2f4d2a630f7e
Himanshu Kharkar <himanshu.kharkar20@gmail.com>
2020-09-29 17:40:12 +0530

    msm8916-common: Add temporary sepolicies for 11 bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -136,6 +136,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += \
 #    $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/minimal-sepolicy
+
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
